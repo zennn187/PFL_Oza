f0b179a1d9cf57c59ad866aa6d1d5c8c4d5fdf8d
ozapcr <oza24si@mahasiswa.pcr.ac.id>
2026-04-13 21:56:36 +0700

    Pertemuan3

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import FormDataDiri from './Pertemuan_3/FormDataDiri';
+
+function App() {
+  return (
+    <div className="App">
+      <FormDataDiri />
+    </div>
+  )
+}
+
+export default App
